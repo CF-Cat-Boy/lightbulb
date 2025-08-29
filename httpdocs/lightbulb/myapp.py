e86b79b47279ac815436a6d9b758b0d20461965f
CF-Cat-Boy <cf.86.cat@gmail.com>
2025-08-29 14:54:17 +1000

    make keygen

--- httpdocs/lightbulb/myapp.py
+++ httpdocs/lightbulb/myapp.py
@@ -7,11 +7,19 @@ app = f.Flask(__name__)
 with open("httpdocs/lightbulb/users.json") as user:
     users = json.load(user)
 
+with open("httpdocs/lightbulb/masterkeyword.txt") as keyword:
+    masterkeyword = str(keyword)
+
+for k, v in users.items():
+    if v["master"] == 1:
+        master = k
+lovk = [i for i in users] # List Of Valid Keys = LOVK
+
 @app.route("/")
 def index():
     key = r.cookies.get("key")
     baddcookie = r.cookies.get("badcookie")
-    if key and key in ["list of valid keys"] and not baddcookie:
+    if key and key in lovk and not baddcookie:
         return f.render_template("index.jinja", args=r.args)
     elif baddcookie:
         badcookie = f.make_response("timer is reset")
@@ -29,17 +37,33 @@ def login():
         return f.render_template("login.jinja")
     elif r.method == "POST":
         if r.form["key"]:
-            if r.form["key"] in ["list of valid keys"]:
+            if r.form["key"] in lovk:
                 keyy = f.make_response("Granting access: Valid key")
                 keyy.set_cookie("key",r.form["key"])
                 return f.redirect("/")
             return f.redirect("#incorrect")
         return f.redirect("#")
 
-@app.route("/keygen",methods=["GET","POST"])
-def keygen():
+@app.route("/keygen/<part>",methods=["GET","POST"])
+def keygen(part):
     # add logic for master key
     if r.method == "GET":
-        pass
+        if part == "main" and r.cookies.get("master") == masterkeyword:
+            return f.render_template("keygen_main.jinja")
+        elif part == "auth":
+            return f.render_template("keygen_auth.jinja")
     elif r.method == "POST":
-        pass # logic for making key
+        if part == "auth":
+            if r.form["auth"] and r.form["auth"] == master:
+                masterr = f.make_response(masterkeyword) # make master-ness cookie
+                masterr.set_cookie("master",masterkeyword,path="/keygen/main")
+                return f.redirect("/keygen/main")
+            else:
+                return f.redirect("/keygen/auth")
+        elif part == "main":
+            return "hi"
+    return f.redirect("/keygen/auth")
+
+@app.route("/keygen/")
+def kk():
+    return f.redirect("/keygen/auth")
