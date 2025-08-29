ea40eff0b6d662376a54ce5cc58806adc99d7286
CF-Cat-Boy <cf.86.cat@gmail.com>
2025-08-29 11:48:42 +1000

    login done

--- httpdocs/lightbulb/myapp.py
+++ httpdocs/lightbulb/myapp.py
@@ -3,30 +3,39 @@ r = f.request
 
 app = f.Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/")
 def index():
-    if r.method == "POST":
+    key = r.cookies.get("key")
+    baddcookie = r.cookies.get("badcookie")
+    if key and key in ["list of valid keys"] and not baddcookie:
+        return f.render_template("index.jinja", args=r.args)
+    elif baddcookie:
+        badcookie = f.make_response("timer is reset")
+        badcookie.set_cookie("badcookie","you-did-a-bad",max_age=3600)
+        return badcookie
+    elif key:
+        badcookie = f.make_response("bad")
+        badcookie.set_cookie("badcookie","you-did-a-bad",max_age=3600)
+        return badcookie
+    return f.redirect("/login/")
+
+@app.route("/login/",methods=["GET","POST"])
+def login():
+    if r.method == "GET":
+        return f.render_template("login.jinja")
+    elif r.method == "POST":
         if r.form["key"]:
             if r.form["key"] in ["list of valid keys"]:
                 keyy = f.make_response("Granting access: Valid key")
                 keyy.set_cookie("key",r.form["key"])
-                return keyy
-        return "y did u send this POST request?"
-    elif r.method == "GET":
-        key = r.cookies.get("key")
-        baddcookie = r.cookies.get("badcookie")
-        if key and key in ["list of valid keys"] and not baddcookie:
-            return f.render_template("index.jinja", args=r.args)
-        elif baddcookie:
-            badcookie = f.make_response("timer is reset")
-            badcookie.set_cookie("badcookie","you-did-a-bad",max_age=3600)
-            return badcookie
-        elif key:
-            badcookie = f.make_response("bad")
-            badcookie.set_cookie("badcookie","you-did-a-bad",max_age=3600)
-            return badcookie
-        return f.abort(401)
+                return f.redirect("/")
+            return f.redirect("#incorrect")
+        return f.redirect("#")
 
 @app.route("/keygen/",methods=["GET","POST"])
 def keygen():
-    pass
+    # add logic for master key
+    if r.method == "GET":
+        pass
+    elif r.method == "POST":
+        pass # logic for making key
