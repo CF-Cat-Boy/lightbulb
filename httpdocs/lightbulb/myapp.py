beaf403d9e00c700c2bbeaa3d6fd59a7b042f879
CF-Cat-Boy <cf.86.cat@gmail.com>
2025-08-29 10:20:11 +1000

    add login page but not yet
    add login page but not done yet
    
    Co-authored-by: deadly-dec <deadly-dec@users.noreply.github.com>

--- httpdocs/lightbulb/myapp.py
+++ httpdocs/lightbulb/myapp.py
@@ -3,23 +3,27 @@ r = f.request
 
 app = f.Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def index():
     key = r.cookies.get("key")
     baddcookie = key = r.cookies.get("badcookie")
-    if key and key in ["list of valid keys"] and not baddcookie:
-        return f.render_template("index.jinja", args=r.args)
-    elif badcookie:
-        badcookie = f.make_response("timer is reset")
-        badcookie.set_cookie("badcookie","you-did-a-bad",max_age=3600)
-        f.abort(401)
-    elif key:
-        badcookie = f.make_response("bad")
-        badcookie.set_cookie("badcookie","you-did-a-bad",max_age=3600)
-        f.abort(401)
-    else:
-        return f.render_template("login.jinja")
+    if r.method == "POST":
+        pass
+    elif r.method == "GET":
+        if key and key in ["list of valid keys"] and not baddcookie:
+            return f.render_template("index.jinja", args=r.args)
+        elif badcookie:
+            badcookie = f.make_response("timer is reset")
+            badcookie.set_cookie("badcookie","you-did-a-bad",max_age=3600)
+            return badcookie
+        elif key:
+            badcookie = f.make_response("bad")
+            badcookie.set_cookie("badcookie","you-did-a-bad",max_age=3600)
+            return badcookie
+        else:
+            print(r.form)
+            return f.render_template("login.jinja")
 
-@app.route("/keygen/")
+@app.route("/keygen/",methods=["GET","POST"])
 def keygen():
-    r.a
+    pass
